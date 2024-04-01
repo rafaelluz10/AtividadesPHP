ceb6ea65bcc4dd70b4a08eb29963a6d63f756433
Rafael Luz Tavares Pereira <rafael.luztp1@gmail.com>
2024-04-01 19:59:28 -0300

    Add files via upload

---
+++ Atividade 3.php
@@ -0,0 +1,22 @@
+<?php
+
+//Rafael Carvalhais e Rafael Luz
+
+
+$nome = "Juliana"; 
+$sexo = "f"; 
+$idade = 25; 
+
+if ($sexo == "f" && $idade <= 25) {
+	echo($nome . ' você foi ACEITA ');
+}
+
+else {
+	echo($nome . ' você NÃO FOI ACEITA ');
+}
+
+
+
+
+
+?>
